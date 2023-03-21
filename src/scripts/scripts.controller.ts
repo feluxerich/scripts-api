e353b79f1b097817a6b8e2d78e410c904bffbaf9
feluxerich <felix.waidler@web.de>
2023-03-21 23:50:55 +0100

    added curl and wget compatibility

--- src/scripts/scripts.controller.ts
+++ src/scripts/scripts.controller.ts
@@ -6,11 +6,14 @@ import {
   Patch,
   Param,
   Delete,
+  ParseUUIDPipe,
+  Req,
 } from '@nestjs/common';
 import { ScriptsService } from './scripts.service';
 import { CreateScriptDto } from './dto/create-script.dto';
 import { UpdateScriptDto } from './dto/update-script.dto';
 import { ApiBody } from '@nestjs/swagger';
+import { Request } from 'express';
 
 @Controller('scripts')
 export class ScriptsController {
@@ -28,18 +31,27 @@ export class ScriptsController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.scriptsService.findOne(id);
+  async findOne(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Req() request: Request,
+  ) {
+    const script = this.scriptsService.findOne(id);
+    if (request.headers['user-agent']?.toLowerCase().match('^(curl|wget)'))
+      return (await script).content;
+    return script;
   }
 
   @Patch(':id')
   @ApiBody({ type: UpdateScriptDto })
-  update(@Param('id') id: string, @Body() updateScriptDto: UpdateScriptDto) {
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateScriptDto: UpdateScriptDto,
+  ) {
     return this.scriptsService.update(id, updateScriptDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.scriptsService.remove(id);
   }
 }
