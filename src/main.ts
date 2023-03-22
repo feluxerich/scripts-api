b8b51538bddcd1a3728f6b331cc24f57fba85392
feluxerich <felix.waidler@web.de>
2023-03-22 13:25:28 +0100

    added validation

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -12,6 +13,12 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
 
   await app.listen(3000);
 }
--- src/scripts/dto/create-script.dto.ts
+++ src/scripts/dto/create-script.dto.ts
@@ -1,12 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, MaxLength } from 'class-validator';
 
 export class CreateScriptDto {
   @ApiProperty()
+  @IsNotEmpty()
   filename: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   content: string;
 
-  @ApiProperty({ nullable: true })
+  @ApiProperty({ nullable: true, maxLength: 128 })
+  @MaxLength(128)
   description: string;
 }
